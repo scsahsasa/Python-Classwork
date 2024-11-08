22cd31dea7803bad9d36d7c4de97ce8f925d6aa8
Atharva <130269449+scsahsasa@users.noreply.github.com>
2024-11-08 16:21:20 +0530

    Add files via upload

---
+++ a4.py
@@ -0,0 +1,11 @@
+for i in range(1,21):
+    if(i==10):
+        break
+    print(i)
+print("-------------------| |-----------------")
+
+for i in range(1,21):
+    if(i==10):
+        print("skipped")
+        continue
+    print(i)
