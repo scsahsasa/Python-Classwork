baa5fdc828105af199ac8e6fcf4aada5d0c7b17a
Atharva <130269449+scsahsasa@users.noreply.github.com>
2024-11-08 16:17:59 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,21 @@
+'''
+age=int(input("Enter age:"))
+if(age>3):
+    print("Eligible for Nursery")
+elif(age>4):
+    print("Eligible for Jr.Kg")
+elif(age>5):
+    print("Eligible for Sr.Kg")
+else:
+    print("Eligible for Play Group")
+'''
+
+age=int(input("Enter age:"))
+if age <=3, age < 4:
+    print("You are eligible for nursery.")
+elif age <=4 age < 5:
+    print("You are eligible for jr.kg.")
+elif age <=5 age <6 :
+    print("You are eligible for sr.kg.")
+else:
+    print("You are eligible for playgroup.")
