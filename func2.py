fb80a08b1ef822bd121f13449b42a2cd2a28f7b8
Atharva <130269449+scsahsasa@users.noreply.github.com>
2024-12-18 18:22:45 +0530

    Add files via upload

---
+++ func2.py
@@ -0,0 +1,15 @@
+#WAP with function to add two numbers
+
+def add(a,b):
+    result=a+b
+    print("The addition is:",result)
+
+add(4,8)
+
+
+#use of return statement
+def add(a,b):
+    result=a+b
+    return result
+add_res=add(14,18)
+print("The addition is",add_res)
---
+++ func3.py
@@ -0,0 +1,8 @@
+length = int(input("Enter a length:"))
+width = int(input("Enter a width:"))
+
+def area(length,width):
+    area=length*width
+    return area
+area=length*width
+print("Area of rectangle is:",area)
